de94236474663203f32bfd5be4751ab8ec6c34d3
Martin Hujer <mhujer@gmail.com>
2017-03-26 20:30:40 +0200

    Dummy/Lazy Uuid

--- mh/benchmark.php
+++ mh/benchmark.php
@@ -2,11 +2,18 @@
 
 declare(strict_types = 1);
 
+use Ramsey\Uuid\Codec\DummyStringCodec;
 use Ramsey\Uuid\Uuid;
+use Ramsey\Uuid\UuidFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $data = require __DIR__ . '/dataset.php';
+
+$f = Uuid::getFactory();
+if ($f instanceof UuidFactory) {
+	$f->setCodec(new DummyStringCodec());
+}
 
 foreach ($data as $uuidString) {
 	$uuid = Uuid::fromString($uuidString);
